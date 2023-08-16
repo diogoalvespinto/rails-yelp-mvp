11c46cac586b607288520fd049e0ce78510845a1
Diogo Pinto <diogo.alvespinto@gmail.com>
2023-08-16 12:32:52 +0100

    seeded restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+myArray = ["chinese", "italian", "japanese", "french", "belgian"]
+
+puts 'Clearing db...'
+Restaurant.delete_all
+puts 'db cleared'
+
+puts 'seeding...'
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: myArray[rand(5)]
+  )
+  restaurant.save!
+  puts "#{restaurant.name} created successfully!"
+end
+puts 'seeding completed!'
